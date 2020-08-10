89f4e71541ad6ecdd963594671a33193eaed867d
Viraj Rajopadhye <59145697+virajrajopadhye@users.noreply.github.com>
2020-08-10 13:34:47 -0400

    Update amibuild.sh

--- amibuild.sh
+++ amibuild.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 
 # ./packer build \
-# -var 'aws_access_key=AKIAXXMPAVJH6UN4UPQD' \
-# -var 'aws_secret_key=fu7MzqjP83AK6CraY3yPJLliucUR3iReewZEONBH' \
+# -var 'aws_access_key=your key' \
+# -var 'aws_secret_key=your key' \
 # -var 'aws_region=us-east-1' \
 # -var 'subnet_id=subnet-1ec0153f' \
 # -var 'ami_description=Ubuntu AMI for Assignment 4' \
